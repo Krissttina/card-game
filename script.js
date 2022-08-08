34584fb21dc40c5326a990bfc87962b349dfbb14
Krissttina <krismetodieva123@gmail.com>
2022-08-08 19:03:47 +0300

    stop script

--- script.js
+++ script.js
@@ -64,3 +64,54 @@ shuffleCard();
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
+
+
+window.onload = function(){
+    var seconds = 00;
+    var tens = 00;
+    var OutputSeconds = document.getElementById("second");
+    var OutputTens = document.getElementById("tens");
+    var buttonStart = document.getElementById("btn-start");
+    var buttonReset = document.getElementById("btn-reset");
+    /*var countStop = document.getElementById("btn-stop");*/
+    var Interval;
+
+    buttonStart.addEventListener('click', () => {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
+    });
+
+    function countStop() {
+        if(f);
+    };
+
+buttonReset.addEventListener('click', () => {
+   clearInterval(Interval);
+    tens = "00";
+    seconds = "00";
+    OutputSeconds.innerHTML = seconds;
+    OutputTens.innerHTML = tens;
+});
+
+    function startTimer(){
+        tens++;
+        if(tens <= 9){
+            OutputTens.innerHTML = "0" + tens;
+        }
+
+        if(tens > 9){
+            OutputTens.innerHTML = tens;
+        }
+
+        if(tens > 99){
+            seconds++;
+            OutputSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            OutputTens.innerHTML = "0" + 0;
+        }
+
+        if(seconds > 9){
+            OutputSeconds.innerHTML = seconds;
+        }
+    }
+} 
